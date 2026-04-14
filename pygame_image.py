ceb7952e3604c14d44080d410f8ce33603bc3087
horie <c0c250688c@edu.teu.ac.jp>
2026-04-14 16:28:53 +0900

    課題1:こうかとんは流れる

--- pygame_image.py
+++ pygame_image.py
@@ -29,7 +29,9 @@ def main():
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip(-1,0)
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(1,0)
+            kk_rct.move_ip(2,0) #課題1.2
+        else:
+            kk_rct.move_ip(-1,0) #課題1.1
         x = tmr%3200 #練習5,9
         screen.blit(bg_img, [-x, 0]) #練習2
         screen.blit(bg_img2, [-x+1600, 0]) #練習7
